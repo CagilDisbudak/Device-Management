51934eef235a7b18aa1979dc6809364dbc2b41ee
Çağıl Dişbudak <cdisbudak07@gmail.com>
2024-04-09 02:00:23 +0300

    Update post_task.py
    Add post_task.py and db.sqllite3

--- case_project/post_task.py
+++ case_project/post_task.py
@@ -1,5 +1,5 @@
 from celery import shared_task
-from CaseProject.models import LocationHistory
+from case_project.models import LocationHistory
 
 @shared_task
 def process_location_data(locations):
