1779326068503f99726fd4430e339c1ca0de8a78
Çağıl Dişbudak <cdisbudak07@gmail.com>
2024-04-09 21:57:36 +0300

    Update Reformat

--- case_project/views.py
+++ case_project/views.py
@@ -1,21 +1,22 @@
 import logging
 from fastapi import FastAPI, HTTPException
 from case_project.models import Device
-from case_project.serializers import LocationHistorySerializer, DeviceSerializer
 from case_project.post_task import process_location_data
 from pydantic import BaseModel
 
-
 app = FastAPI()
+
 
 class DeviceCreate(BaseModel):
     name: str
+
 
 @app.post("/devices/create/")
 def create_device(device_data: DeviceCreate):
     new_device = Device.objects.create(name=device_data.name)
     logging.info(f"Created device: {new_device.id}, Name: {new_device.name}")
     return {"id": new_device.id, "name": new_device.name}
+
 
 @app.post("/devices/{device_id}/add_location/", response_model=None)
 def add_location(device_id: int, location_data):
@@ -28,6 +29,7 @@ def add_location(device_id: int, location_data):
     process_location_data.delay([serialized_data])
     logging.info(f"Added location for device {device_id}: {serialized_data}")
 
+
 @app.delete("/devices/{device_id}/delete/")
 def delete_device(device_id: int):
     device = Device.objects.filter(id=device_id).first()
@@ -37,11 +39,13 @@ def delete_device(device_id: int):
     logging.info(f"Deleted device: {device_id}")
     return {"message": "Device deleted successfully"}
 
+
 @app.get("/devices/list/")
 def list_devices():
     devices = Device.objects.all()
     logging.info("Listed all devices")
     return devices
+
 
 @app.get("/devices/{device_id}/location/history/")
 def list_location_history(device_id: int):
@@ -52,12 +56,14 @@ def list_location_history(device_id: int):
     logging.info(f"Listed location history for device {device_id}")
     return history
 
+
 @app.get("/devices/location/last/")
 def get_last_location_for_all_devices():
     devices = Device.objects.all()
     last_locations = []
     for device in devices:
         last_location = device.location_history.order_by('-timestamp').first()
-        last_locations.append({"device_id": device.id, "latitude": last_location.latitude, "longitude": last_location.longitude})
+        last_locations.append(
+            {"device_id": device.id, "latitude": last_location.latitude, "longitude": last_location.longitude})
         logging.info(f"Retrieved last location for device {device.id}")
     return last_locations
