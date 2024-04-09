78cb9827b425fa9da3ce10e06c1de3d4b84617c6
Çağıl Dişbudak <cdisbudak07@gmail.com>
2024-04-09 18:20:06 +0300

    Create unit_test.py
    Add UT

---
+++ test/unit_test.py
@@ -0,0 +1,32 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from rest_framework.test import APIRequestFactory
+from case_project.models import Device, Location
+
+class DeviceViewsTestCase(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+
+    def test_create_device(self):
+        response = self.factory.post(('create_device'), {'name': 'Test Device'})
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_location(self):
+        response = self.factory.post(reverse('add_location', kwargs={'device_id': 1}), {'latitude': 123.456, 'longitude': 78.90})
+        self.assertEqual(response.status_code, 404)  # Assuming device with ID 1 doesn't exist
+
+    def test_delete_device(self):
+        response = self.factory.delete('delete_device', kwargs={'device_id': 1})
+        self.assertEqual(response.status_code, 404)  # Assuming device with ID 1 doesn't exist
+
+    def test_list_devices(self):
+        response = self.factory.get('list_devices')
+        self.assertEqual(response.status_code, 200)
+
+    def test_list_location_history(self):
+        response = self.factory.get('list_location_history', kwargs={'device_id': 1})
+        self.assertEqual(response.status_code, 404)  # Assuming device with ID 1 doesn't exist
+
+    def test_get_last_location_for_all_devices(self):
+        response = self.factory.get('get_last_location_for_all_devices')
+        self.assertEqual(response.status_code, 200)
